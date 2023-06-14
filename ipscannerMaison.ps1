46fafd0ea3d279138658854bca9d8ef31de5cfb7
Guns51 <gunsalexandre@gmail.com>
2023-06-14 21:56:52 +0200

    fff

--- ipscannerMaison.ps1
+++ ipscannerMaison.ps1
@@ -2,18 +2,18 @@
 
 1..254 | % -Parallel {
     #$status = Test-Connection -TargetName 192.168.191.$_ -Count 1
-    if (Test-Connection -TargetName 192.168.6.$_ -Count 1 -Quiet)
+    if (Test-Connection -TargetName 192.168.1.$_ -Count 1 -Quiet)
     {	
-        $dnsName = (Resolve-DnsName 192.168.6.$_ -ErrorAction SilentlyContinue).NameHost
+        $dnsName = (Resolve-DnsName 192.168.1.$_ -ErrorAction SilentlyContinue).NameHost
         if ($dnsName -eq $null)
         {
             $dnsName = "<notDnsName>"
         }
-	    $resultTestSSH = Test-NetConnection -ComputerName 192.168.6.$_ -Port 22 -InformationLevel Quiet -WarningAction SilentlyContinue
+	    $resultTestSSH = Test-NetConnection -ComputerName 192.168.1.$_ -Port 22 -InformationLevel Quiet -WarningAction SilentlyContinue
         if ($resultTestSSH)
         {
             $resultTestSSH = "OK:22"
         }
-        Write-Host "Success > 192.168.6.$_ > $dnsName > $resultTestSSH"
+        Write-Host "Success > 192.168.1.$_ > $dnsName > $resultTestSSH"
     }
 } -ThrottleLimit 150
