5d1f6c2a7da9e2c99f5dc1a389096227de129393
Guns51 <gunsalexandre@gmail.com>
2023-12-01 17:41:37 +0100

    zy

---
+++ getIpPub.ps1
@@ -0,0 +1,18 @@
+﻿$test = Invoke-WebRequest -UseBasicParsing https://www.monippublique.com/
+
+$test = $test.Content
+$chaine1= '<h1>Votre IP publique est : <span class="big-green">'
+$chaine2 = '</span></h1>'
+$contenu = $test.IndexOf($chaine1)
+$longueurdebut = $chaine1.Length
+$in = $contenu + $longueurdebut
+$contenu2 = $test.IndexOf($chaine2)
+$result = $contenu2-$in
+$f = $test.Substring($in,$result)
+Write-Host "||||||||||||||||||||||||||||||||||||||||||||
+|||||||||||||||||||||||||||||||||||||||||||||||||||"
+Write-Host "Votre ip publique : " $f 
+Write-Host "|||||||||||||||||||||||||||||||||||||||||||||||
+
+|||||||||||||||||||||||||||||||||||||||||||||||"
+pause
